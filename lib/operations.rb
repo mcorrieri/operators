becd892f2be3f00abeab061429e5d8e162c60556
Michael <Mlcorrieri@gmail.com>
2021-04-25 18:39:57 -0400

    commit tree e8ede37e9ff5fd75ea120001a755c5e6646be062

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
+    if speed > 60 && speed < 40
+        return true 
+    else 
+        return false
 
 end
 
 
 
-def not_safe?(speed)
+# def not_safe?(speed)
 	
-end
+# end
 	
 
 
